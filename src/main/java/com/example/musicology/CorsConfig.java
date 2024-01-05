e8f38e081b28ff80fa6b1b738b6be8904efce858
IVAN BARRIGA FUENTES <ivanbf@gmail.com>
2024-01-05 12:56:38 +0100

    prueba cors

--- src/main/java/com/example/musicology/CorsConfig.java
+++ src/main/java/com/example/musicology/CorsConfig.java
@@ -12,13 +12,27 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://musicology-front.web.app");
-        config.addAllowedOrigin("https://musicology-front.firebaseapp.com");
+        config.addAllowedOrigin("*"); // O config.addAllowedOrigin("https://musicology-front.web.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+//
+//        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedOrigin("https://musicology-front.web.app");
+//        config.addAllowedOrigin("https://musicology-front.firebaseapp.com");
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 }
