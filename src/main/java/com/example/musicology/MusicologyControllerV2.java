cb78302151588063cd2423a60941819345be2e7c
IVAN BARRIGA FUENTES <ivanbf@gmail.com>
2024-01-08 13:18:26 +0100

    controles manuales.

--- src/main/java/com/example/musicology/MusicologyControllerV2.java
+++ src/main/java/com/example/musicology/MusicologyControllerV2.java
@@ -1,5 +1,6 @@
 package com.example.musicology;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Log4j2
 @RequestMapping("/v2")
 @CrossOrigin(origins = {"http://localhost:3000", "https://musicology-front.web.app", "https://musicology-front.firebaseapp.com"})
 //@CrossOrigin(origins = {"http://localhost:3000", "https://musicology-front.web.app:3000", "https://musicology-front.firebaseapp.com:3000"})
@@ -35,13 +37,16 @@ public class MusicologyControllerV2 {
     @GetMapping("/registros-options")
     public ResponseEntity<?> handleOptionsRequest() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        headers.add("Access-Control-Allow-Headers", "*");
-        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Origin", "http://localhost:3000, https://musicology-front.web.app, https://musicology-front.firebaseapp.com");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Origin");
+        //headers.add("Access-Control-Allow-Credentials", "true");
         headers.add("Access-Control-Max-Age", "3600");
+        log.info("OPTIONS request received");
 
-        return ResponseEntity.ok().headers(headers).build();
+        var responseEntity =  ResponseEntity.ok().headers(headers).build();
+
+        return responseEntity;
     }
 
     @GetMapping("/nuevo-registro")
